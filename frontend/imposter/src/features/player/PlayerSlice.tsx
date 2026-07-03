a1c776c2ca16071c265e6d3cbb248df700625e3b
Eyad Mahmoud <139317529+EyadMohamed919@users.noreply.github.com>
2026-07-03 15:37:04 +0300

    added local storage managment

--- frontend/imposter/src/features/player/PlayerSlice.tsx
+++ frontend/imposter/src/features/player/PlayerSlice.tsx
@@ -1,29 +1,33 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
-interface game{
-    id:number,
-    category:string,
-    topic:string
+interface game {
+    id: number,
+    category: string,
+    topic: string
 }
 interface player {
     id: number | null,
     name: string | null,
-    imposter:boolean,
-    game:game | null,
+    imposter: boolean,
+    game: game | null,
     votedPlayer: player | null
 }
 
-const initialState: player = {
-    id:null,
-    name:null,
-    imposter:false,
-    game:null,
-    votedPlayer: null
-};
+const storedPlayer = localStorage.getItem("player");
+
+const initialState: player = storedPlayer 
+    ? JSON.parse(storedPlayer) 
+    : {
+        id: null,
+        name: null,
+        imposter: false,
+        game: null,
+        votedPlayer: null
+      };
 
 export const PlayerSlice = createSlice({
-    name:"player",
-    initialState:initialState,
+    name: "player",
+    initialState: initialState, 
     reducers: {
         attachPlayer: (state, action: PayloadAction<player>) => {
             state.id = action.payload.id;
@@ -31,17 +35,21 @@ export const PlayerSlice = createSlice({
             state.imposter = action.payload.imposter;
             state.game = action.payload.game;
             state.votedPlayer = action.payload.votedPlayer;
+
+            localStorage.setItem("player", JSON.stringify(action.payload));
         },
         detachPlayer: (state) => {
             state.id = null;
             state.name = null;
             state.imposter = false;
             state.game = null;
-            state.votedPlayer = null;            
+            state.votedPlayer = null;    
+            
+            localStorage.removeItem("player");
         }
     }
 });
 
-export const {attachPlayer, detachPlayer} = PlayerSlice.actions;
+export const { attachPlayer, detachPlayer } = PlayerSlice.actions;
 
 export default PlayerSlice.reducer;
